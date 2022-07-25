b9dbc4b1376b66a49e4bbe61ee1adaf19b67e0a4
wjchoi96 <wjchoi96@naver.com>
2022-07-26 02:02:09 +0900

    [Feature] codelab 8단계 완료

--- MigrationCodelabPractice/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ MigrationCodelabPractice/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -16,13 +16,85 @@
 
 package com.google.samples.apps.sunflower.plantdetail
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.google.samples.apps.sunflower.R
+import com.google.samples.apps.sunflower.data.Plant
+import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
 
 @Composable
-fun PlantDetailDescription() {
+fun PlantDetailDescription(viewModel: PlantDetailViewModel) {
+    val plant by viewModel.plant.observeAsState()
+    plant?.let {
+        PlantDetailContent(it)
+    }
+}
+
+@Composable
+private fun PlantDetailContent(plant: Plant){
     Surface {
-        Text("Hello Compose")
+        Column(
+            modifier = Modifier.padding(dimensionResource(id = R.dimen.margin_normal))
+        ) {
+            PlantName(name = plant.name)
+            PlantWatering(wateringInterval = plant.wateringInterval)
+        }
+    }
+}
+
+@Composable
+private fun PlantName(name: String){
+    Text(
+        text = name,
+        style = MaterialTheme.typography.h5,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = dimensionResource(id = R.dimen.margin_small))
+            .wrapContentWidth(Alignment.CenterHorizontally) // text 를 가로로 정렬
+    )
+}
+
+@Composable
+private fun PlantWatering(wateringInterval: Int){
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        val centerWithPadding = Modifier
+            .padding(horizontal = dimensionResource(id = R.dimen.margin_small))
+            .align(Alignment.CenterHorizontally)
+        Text(
+            text = stringResource(id = R.string.watering_needs_prefix),
+            modifier = centerWithPadding.padding(top = dimensionResource(id = R.dimen.margin_normal)),
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colors.primaryVariant // colorAccent 와 일치하는건 아니지만, 추후 테마 설정때 수정 예정
+        )
+        Text(
+            text = "every $wateringInterval days",
+            modifier = centerWithPadding
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun PlantNamePreview(){
+    val plant = Plant("id", "Apple", "description", 3, 30, "")
+    MaterialTheme {
+        PlantDetailContent(plant)
     }
 }
